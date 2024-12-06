bfeb2219094cd86919ae1a7d3aca5c3c26b0c1a6
Sammy Gros <144098583+wamyr@users.noreply.github.com>
2024-12-06 04:29:32 +0100

    Add files via upload

---
+++ random_verse (copy).sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Chemin du fichier versets.txt
+FILE_PATH="/path/to/your/versets.txt"  # Modifiez ceci avec le chemin réel de votre fichier versets.txt
+
+# Exécute le script Python et affiche un verset aléatoire
+python3 /home/sammygros/versets/random_verse.py
